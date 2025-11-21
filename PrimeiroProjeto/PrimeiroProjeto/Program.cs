3dda76edf7c0a73fc457980e78f21e2500e41622
iKripto <yagonascimento0288@gmail.com>
2025-11-21 16:03:12 -0300

    If, Else, Switch Case.

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -1,4 +1,41 @@
 ﻿// Screen Sound
-string mensagemDeBoasVindas = "Boas vindas ao Screen Sound!";
-Console.WriteLine(mensagemDeBoasVindas);
-Console.WriteLine('A');
+
+void ExibirBoasVindas()
+{
+    Console.WriteLine(@"
+            
+        ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+        ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+        ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+        ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+        ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+        ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+        Boas vindas ao Screen Sound.
+    ");
+}
+
+void exibirOpcoesDoMenu()
+{
+    Console.WriteLine("Digite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para Sair");
+    Console.Write("Escolha uma opção: ");
+
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: Console.WriteLine("Você escolheu a opção 1" ); break;
+        case 2: Console.WriteLine("Você escolheu a opção 2" ); break;
+        case 3: Console.WriteLine("Você escolheu a opção 3" ); break;
+        case 4: Console.WriteLine("Você escolheu a opção 4" ); break;  
+        case -1: Console.WriteLine("Você escolheu sair" ); break; 
+        default: Console.WriteLine("Opção Inválida!"); break;
+    }
+
+}
+
+ExibirBoasVindas();
+exibirOpcoesDoMenu();
